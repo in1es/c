b622f8ede5c75d59b0d0e6bd2da343f9187dac49
in1es <49292740+in1es@users.noreply.github.com>
2023-11-17 11:28:52 +0100

    Update complex_numbers.c

--- exercises/practice/complex-numbers/complex_numbers.c
+++ exercises/practice/complex-numbers/complex_numbers.c
@@ -2,45 +2,62 @@
 
 complex_t c_add(complex_t a, complex_t b)
 {
-   // TODO: implement
+   complex_t = z;
+   z.real = a.real + b.real;
+   z.imag = a.imag + b.imag;
+   return z;
 }
 
 complex_t c_sub(complex_t a, complex_t b)
 {
-   // TODO: implement
+   complex_t = t;
+   z.real = a.real - b.real;
+   z.imag = a.imag - b.imag;
+   return z;
 }
 
 complex_t c_mul(complex_t a, complex_t b)
 {
-   // TODO: implement
+   complex_t = z;
+   z.real = a.real * b.real -  a.imag * b.imag;
+   z.imag = a.real * b.imag + a.imag * b.real;
+   return z;
 }
 
 complex_t c_div(complex_t a, complex_t b)
 {
-   // TODO: implement
+    complex_t = z;
+    z.real = (a.real * b.real + a.imag * b.imag) / ( b.imag * b.imag + b.real * b.real);
+    z.imag = (a.imag * b.real - a.real * b.imag) / ( b.imag * b.imag + b.real * b.real);
+    return z;
 }
 
 double c_abs(complex_t x)
 {
-   // TODO: implement
+    return sqrt(x.real * x.real + x.imag * x.imag);
 }
 
 complex_t c_conjugate(complex_t x)
 {
-   // TODO: implement
+    x.imag = -x.imag;
+    return x;
 }
 
 double c_real(complex_t x)
 {
-   // TODO: implement
+   return x.real;
 }
 
 double c_imag(complex_t x)
 {
-   // TODO: implement
+   return x.imag;
 }
 
 complex_t c_exp(complex_t x)
 {
-   // TODO: implement
+     double expA = exp(x.real);
+     return (complex_t) {
+            .real = expA * cos(x.imag),
+            .imag = expA * sin(x.imag)
+    };
 }
